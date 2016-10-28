537c1f4f0080dadb2325168eab49fea3f90f378c
andsaltos <andreseoch.saltos@uteq.edu.ec>
2016-10-28 17:16:37 -0500

    interfaz del programa

---
+++ mensajeriaInstantanea/src/mensajeriainstantanea/MensajeriaInstantanea.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ package mensajeriainstantanea;
+
+import vista.*;
+/**
+ *
+ * @author ochoa
+ */
+public class MensajeriaInstantanea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventanaPrincipal ventana = new ventanaPrincipal();
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+    }
+    
+
+}
